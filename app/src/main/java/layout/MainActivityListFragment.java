65d50ee6cfe63cdecf65df7acafc140474ff1faa
Jayden Spencer <jaydenspencer1@gmail.com>
2016-04-21 20:15:15 +0800

    Initial Commit

---
+++ app/src/main/java/layout/MainActivityListFragment.java
@@ -0,0 +1,31 @@
+package layout;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.ListFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.jayden.moviebase.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class MainActivityListFragment extends ListFragment {
+
+
+    public MainActivityListFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.list_row, container, false);
+    }
+
+}
